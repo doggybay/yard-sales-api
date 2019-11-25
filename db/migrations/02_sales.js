82c75c61d8bf3f2b84f2212b070af6486007e427
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-24 22:32:26 -0700

    creating migrations

---
+++ db/migrations/02_sales.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('sales', table => {
+    table.increments();
+    table.string('title').notNullable();
+    table.text('details').notNullable();
+    table.integer('user_id').references('users.id').onDelete('CASCADE');
+    table.string('location').notNullable();
+    table.datetime('date_time').unique().notNullable();
+
+  })
+};
+
+exports.down = function(knex) {
+  
+};
