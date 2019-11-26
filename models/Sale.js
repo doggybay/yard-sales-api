41522749b0b64912a2eff18e82e4d9a0118f84eb
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-26 09:52:16 -0700

    completing models

--- models/Sale.js
+++ models/Sale.js
@@ -1,2 +1,40 @@
 const { Model } = require('objection');
 
+class Sale extends Model {
+  static get tableName() {
+    return 'sales'
+  }
+
+  static get relationMappings() {
+    const SalePicture = ('./SalePicture')
+    return {
+      pictures: {
+        relation: Model.HasManyRelation,
+        modelClass: SalePicture,
+        join: {
+          from: 'sales.id',
+          to: 'sales_pictures.sale_id'
+        }
+      }
+    }
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['title', 'user_id', 'location', 'date_time'],
+
+      properties: {
+        id: { type: 'integer' },
+        title: { type: 'string', minLength: 1, maxLength: 255 },
+        details: { type: 'string' },
+        user_id: { type: 'integer' },
+        location: { type: 'string', minLength: 1, maxLength: 255 },
+        date_time: { type: 'string', minLength: 1, maxLength: 255}
+      }
+    }
+  }
+
+}
+
+module.exports = Sale
---
+++ models/SalePicture.js
@@ -0,0 +1,23 @@
+const { Model } = require('objection');
+
+class SalePicture extends Model {
+  static get tableName() {
+    return 'sales_pictures'
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      reuired: ['sale_id', 'pic'],
+
+      properties: {
+        id: { type: 'integer' },
+        sale_id: { type: 'integer' },
+        pic: { type: 'string' }
+      }
+    }
+  }
+
+}
+
+module.exports = SalePicture
