52c57a7d6e116e67b6dff37f8493f11a12549a7b
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-26 20:54:03 -0700

    completing picture routes

--- controllers/sales_pictures.js
+++ controllers/sales_pictures.js
@@ -3,3 +3,15 @@ const SalePicture = require('../models/Sale_Picture')
 exports.getAllPictures = (req, res) => {
   SalePicture.query().then(salesPictures => res.json(salesPictures))
 }
+
+exports.getOnePicture = (req, res) => {
+  SalePicture.query().findById(req.params.id).then(salePicture => res.json(salePicture))
+}
+
+exports.deleteOnePicture = (req, res) => {
+  SalePicture.query().deleteById(req.params.id).returning('*').then(deletedPicture => res.json(deletedPicture))
+}
+
+exports.addOnePicture = (req, res) => {
+  SalePicture.query().insert(req.body).returning('*').then(newPicture => res.json(newPicture))
+}
