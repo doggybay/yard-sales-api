f8cbba81c1c076a7695253454e01e6b495f1c413
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-26 18:11:30 -0700

    completed user routes

--- controllers/users.js
+++ controllers/users.js
@@ -7,3 +7,15 @@ exports.getAllUsers = (req, res) => {
 exports.getOneUser = (req, res) => {
   User.query().findById(req.params.id).eager('sales').then(user => res.json(user))
 }
+
+exports.updateOneUser = (req, res) => {
+  User.query().findById(req.params.id).patch(re.body).returning('*').then(updatedUser => res.json(updatedUser))
+}
+
+exports.deleteOneUser = (req, res) => {
+  User.query().deleteById(req.params.id).returning('*').then(deletedUser => res.json(deletedUser))
+}
+
+exports.addOneUser = (req, res) => {
+  User.query().insert(req.body).returning('*').then(newUser => res.json(newUser))
+}
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,17 @@ const usersController = require('../controllers/users')
 
 //Get all users
 router.get('/users', usersController.getAllUsers)
+
+//Get one user
 router.get('/users/:id', usersController.getOneUser)
 
+//Add one user
+router.post('/users', usersController.addOneUser)
+
+//Update one user
+router.patch('/users/:id', usersController.updateOneUser)
+
+//Delete one user
+router.delete('/users/:id', usersController.deleteOneUser)
 
 module.exports = router
