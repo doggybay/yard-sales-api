1aa56f8b330c590452e9723613fdd0d67b07c721
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-12-17 21:42:07 -0700

    working on update sale route

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,5 +1,6 @@
 const Sale = require('../models/Sale')
 const knex = require('../db/knex')
+const picturesController = require('../controllers/pictures')
 
 exports.getAllSales = async (req, res) => {
   Sale.query().eager('pictures').then(sales => res.json(sales))
@@ -10,7 +11,37 @@ exports.getOneSale = (req, res) => {
 }
 
 exports.updateOneSale = (req, res) => {
-  Sale.query().findById(req.params.id).patch(req.body).returning('*').then(updatedSale => res.json(updatedSale))
+  let sale = req.body
+  let saleToUpdate = {
+    user_id: sale.user_id,
+    title: sale.title,
+    details: sale.details,
+    location: sale.location,
+    date_time: sale.date_time
+  }
+
+  Sale.query().findById(req.params.id).patch(saleToUpdate).returning('*').then(updatedSale => {
+    let id = updatedSale.id
+    let pictures = sale.pictures
+
+    // Looping through the pictures array obtained from req body
+    for (let i = 0; i < pictures.length; i++) {
+      if (!pictures[i].hasOwnProperty('id')) {
+        // Creating the new picture object
+      let formattedPic = {
+        sale_id: id,
+        pic: pictures[i]
+      }
+
+      // Inserting the new picture object into the pictures table
+        knex('pictures').insert(formattedPic).returning('*').then(result => result)
+      }
+    }
+
+    // Sending the new sale back
+    res.json(updatedSale)
+  })
+
 }
 
 exports.deleteOneSale = (req, res) => {
