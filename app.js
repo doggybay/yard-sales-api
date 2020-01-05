67bb171b81cb8544db9348c5454facded3b25dad
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-01-05 11:57:07 -0700

    adding dotenv

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv')
 const { app } = require('./server');
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 app.listen(process.env.PORT, () => console.log('Listening on port: ', process.env.PORT));
